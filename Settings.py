af4089948e064db5e6bc2a2d197e2d392aed57cf
Ronen Singer <ronensinger17@gmail.com>
2018-01-11 22:01:47 -0500

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,8 +1,8 @@
 url = 'wss://ws-api.coincheck.com/'
 minimum_yen_spread = 1
 increment = 5 #10
-access_token = 'LRM_Pw0_ZNLRW3ie'
-access_key = 'y89yuL_VNndfKIfnP_jM2qYjl41zsDwx'
+access_token = ''
+access_key = ''
 bitcoin = 0.1
 max_position = 0.02
 order_size = 0.01
